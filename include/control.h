af5e2b7ad230da0d855ab869b092648937b4c57e
jcoli <jefersoncoli@gmail.com>
2023-07-19 10:10:56 -0300

    190723-1010

---
+++ include/control.h
@@ -0,0 +1,18 @@
+/**
+Version: 0a
+Tecnocoli - 06/2023
+jcoli - Jeferson Coli - jcoli@teccnocoli.com.br
+STM32F401 - Mio Stimulation
+**/
+
+#ifndef __CONTROL_H__
+#define __CONTROL_H__
+
+
+
+
+
+
+
+
+#endif
---
+++ src/control.cpp
@@ -0,0 +1,23 @@
+/**
+Version: 0a
+Tecnocoli - 06/2023
+jcoli - Jeferson Coli - jcoli@teccnocoli.com.br
+STM32F401 - Mio Stimulation
+**/
+
+#include <Arduino.h>
+
+#include "defines.h"
+#include "io_defines.h"
+#include "tools.h"
+#include "communication.h"
+
+extern bool bt_enabled;
+extern bool bt_connected;
+
+extern String retMsg2[2];
+extern String retMsg3[3];
+extern String retMsg4[4];
+extern String retMsg5[5]; 
+
+
