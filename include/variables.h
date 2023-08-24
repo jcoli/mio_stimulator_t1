880a82d8b1928981a126275ae635c56d1076e709
jcoli <jefersoncoli@gmail.com>
2023-08-24 18:21:53 -0300

    240823-1821

--- include/variables.h
+++ include/variables.h
@@ -43,5 +43,46 @@ float batCel2Level;
 
 volatile int repetitions = 1;
 
+//Channel 0
+bool active_ch0 = false;
+bool detect_ch0 = false;
+int intensity_ch0 = 0;
+
+//Channel 1
+bool active_ch1 = false;
+bool detect_ch1 = false;
+int intensity_ch1 = 0;
+
+//Channel 2
+bool active_ch2 = false;
+bool detect_ch2 = false;
+int intensity_ch2 = 0;
+
+//Channel 3
+bool active_ch3 = false;
+bool detect_ch3 = false;
+int intensity_ch3 = 0;
+
+//Channel 4
+bool active_ch4 = false;
+bool detect_ch4 = false;
+int intensity_ch4 = 0;
+
+//Channel 5
+bool active_ch5 = false;
+bool detect_ch5 = false;
+int intensity_ch5 = 0;
+
+//Channel 6
+bool active_ch6 = false;
+bool detect_ch6 = false;
+int intensity_ch6 = 0;
+
+//Channel 7
+bool active_ch7 = false;
+bool detect_ch7 = false;
+int intensity_ch7 = 0;
+
+
 
 #endif
--- src/pulse_control.cpp
+++ src/pulse_control.cpp
@@ -22,12 +22,44 @@ void stepUp_block();
 extern bool bt_enabled;
 extern bool bt_connected;
 extern bool bt_alive;
-
 extern bool test;
-
 extern int tim_alive;
 extern int tim_conn;
 extern int tim_sleep;
+
+//Channel 0
+extern bool active_ch0;
+extern bool detect_ch0;
+extern int intensity_ch0;
+//Channel 1
+extern bool active_ch1;
+extern bool detect_ch1;
+extern int intensity_ch1;
+//Channel 2
+extern bool active_ch2;
+extern bool detect_ch2;
+extern int intensity_ch2;
+//Channel 3
+extern bool active_ch3;
+extern bool detect_ch3;
+extern int intensity_ch3;
+//Channel 4
+extern bool active_ch4;
+extern bool detect_ch4;
+extern int intensity_ch4;
+//Channel 5
+extern bool active_ch5;
+extern bool detect_ch5;
+extern int intensity_ch5;
+//Channel 6
+extern bool active_ch6;
+extern bool detect_ch6;
+extern int intensity_ch6;
+//Channel 7
+extern bool active_ch7;
+extern bool detect_ch7;
+extern int intensity_ch7;
+
 
 
 void pulse_init(){
